2395e589d536278a524ea4b5d5108d91d5dda338
Lou007 <kooljonez@yahoo.com>
2014-11-19 14:15:24 -0600

    testing non flawed file

---
+++ TestDocs/returns0.cpp
@@ -0,0 +1,37 @@
+//DEFINE COPYRIGHT IN THIS FILE
+
+
+
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    while( 1 )
+    {
+        a = a % b;
+          if( a == 0 )
+               return b;
+          b = b % a;
+
+        if( b == 0 )
+               return a;
+    }
+}
+
+int main()
+{
+     int x, y;
+
+     cout << "This program allows calculating the GCD\n";
+     cout << "Value 1: ";
+     cin >> x;
+     cout << "Value 2: ";
+     cin >> y;
+
+     cout << "\nThe Greatest Common Divisor of "
+          << x << " and " << y << " is " << GCD(x, y) << endl;  
+
+     return 0;
+}
+
